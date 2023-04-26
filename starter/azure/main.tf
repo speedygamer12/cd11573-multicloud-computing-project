e0c22f50d0d1caa730b05c88a7732c2010775810
Yusuf Giwa <giwayusuf12@gmail.com>
2023-04-26 08:24:41 +0100

    chore
    Signed-off-by: Yusuf Giwa <giwayusuf12@gmail.com>

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_container_group" "udacity" {
 ####### Your Additions Will Start Here ######
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplesa"
+  name                     = "yusuf-udacity-storage"
   resource_group_name      = data.azurerm_resource_group.udacity.name
   location                 = data.azurerm_resource_group.udacity.location
   account_tier             = "Standard"
